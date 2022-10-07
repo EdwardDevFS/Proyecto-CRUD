b50e73889c91ba4722e215b5367e359073c352bf
Edward <Edward_Dev@outlook.com>
2022-10-07 13:52:40 -0500

    Commit: Trabajando CRUD con C# Y SQL Server

---
+++ Sistema.Datos/Conexion.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//agregar
+using System.Data.SqlClient;
+
+namespace Sistema.Datos
+{
+    class Conexion
+    {
+        private static Conexion Con = null;
+
+        private Conexion()
+        {
+
+        }
+
+        public SqlConnection CrearConexion()
+        {
+            SqlConnection Cadena = new SqlConnection();
+            try
+            {
+                Cadena.ConnectionString = "Data Source=DESKTOP-6A9C73D\\EDWARD; Initial Catalog=dbsistema;Integrated Security=True";
+            }
+            catch(Exception ex)
+            {
+                Cadena = null;
+                throw ex;
+            }
+            return Cadena;
+        }
+
+        public static Conexion getInstancia()
+        {
+            if(Con == null)
+            {
+                Con = new Conexion();
+            }
+            return Con;
+        }
+    }
+}
---
+++ Sistema.Presentacion/FrmArticulo.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Import
+using Sistema.Negocio;
+
+namespace Sistema.Presentacion
+{
+    public partial class FrmArticulo : Form
+    {
+        public FrmArticulo()
+        {
+            InitializeComponent();
+        }
+
+        private void Listar()
+        {
+            try
+            {
+                DgvListado.DataSource = NArticulo.Listar();
+                this.Formato();
+                this.Limpiar();
+                LblTotal.Text = "Total registros: " + Convert.ToString(DgvListado.Rows.Count);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+        }
+
+        private void Buscar()
+        {
+            try
+            {
+                DgvListado.DataSource = NArticulo.Buscar(TxtBuscar.Text);
+                this.Formato();
+                LblTotal.Text = "Total registros: " + Convert.ToString(DgvListado.Rows.Count);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+        }
+
+        private void Formato()
+        {
+            DgvListado.Columns[0].Visible = false;
+            DgvListado.Columns[0].Width = 100;
+            DgvListado.Columns[1].Width = 50;
+            DgvListado.Columns[2].Visible = false;
+            DgvListado.Columns[3].Width = 100;
+            DgvListado.Columns[3].HeaderText = "Categoría";
+            DgvListado.Columns[4].Width = 100;
+            DgvListado.Columns[4].HeaderText = "Código";
+            DgvListado.Columns[5].Width = 150;
+            DgvListado.Columns[6].Width = 100;
+            DgvListado.Columns[6].HeaderText = "Precio Venta";
+            DgvListado.Columns[7].Width = 60;
+            DgvListado.Columns[8].Width = 200;
+            DgvListado.Columns[8].HeaderText = "Descripción";
+            DgvListado.Columns[9].Width = 100;
+            DgvListado.Columns[10].Width = 100;
+        }
+
+        private void Limpiar()
+        {
+            TxtBuscar.Clear();
+            TxtNombre.Clear();
+            TxtId.Clear();
+            TxtDescripcion.Clear();
+            BtnInsertar.Visible = true;
+            BtnActualizar.Visible = false;
+            ErrorIcono.Clear();
+
+            DgvListado.Columns[0].Visible = false;
+            BtnActivar.Visible = false;
+            BtnDesactivar.Visible = false;
+            BtnEliminar.Visible = false;
+        }
+
+        private void MensajeError(string Mensaje)
+        {
+            MessageBox.Show(Mensaje, "Sistema de Ventas", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        private void MensajeOK(string Mensaje)
+        {
+            MessageBox.Show(Mensaje, "Sistema de Ventas", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void CargarCategoria()
+        {
+            try
+            {
+                CboCategoria.DataSource = NCategoria.Seleccionar();
+                CboCategoria.ValueMember = "idcategoria";
+                CboCategoria.DisplayMember = "nombre";
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message + ex.StackTrace);
+            }
+        }
+
+        private void FrmArticulo_Load(object sender, EventArgs e)
+        {
+            this.Listar();
+            this.CargarCategoria();
+        }
+
+        private void BtnBuscar_Click(object sender, EventArgs e)
+        {
+            this.Buscar();
+        }
+
+        private void FrmArticulo_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            FrmPrincipal.ventana_articulo_open = false;
+        }
+    }
+}
